a5f2f671e9fae9e719aa0bc4dc5c83d208583b1a
Dudo1985 <d.curvino@gmail.com>
2023-04-12 17:01:06 +0200

    PhpDocumentor has now static members

--- src/PhpDocumentor.php
+++ src/PhpDocumentor.php
@@ -6,53 +6,66 @@
  */
 namespace Dudo1985\WPDocGen;
 
-class PhpDocumentor {
-    public array $tags = array (
-        '@abstract',
-        '@access',
-        '@author',
-        '@category',
-        '@copyright',
-        '@deprecated',
-        '@example',
-        '@final',
-        '@filesource',
-        '@global',
-        '@ignore',
-        '@internal',
-        '@license',
-        '@link',
-        '@method',
-        '@name',
-        '@package',
-        '@param',
-        '@property',
-        '@return',
-        '@see',
-        '@since',
-        '@static',
-        '@staticvar',
-        '@subpackage',
-        '@todo',
-        '@tutorial',
-        '@uses',
-        '@var',
-        '@version'
-    );
+if (!class_exists('Dudo1985\WPDocGen\PhpDocumentor')) {
 
-    /**
-     * Return true if the given string is a phpDocTag, false otherwise
-     *
-     * @param $string
-     * @return bool
-     * @author Dario Curvino <@dudo>
-     *
-     * @since 2.0.2
-     */
-    public function isTag ($string): bool {
-        if (in_array($string, $this->tags, true)) {
-            return true;
+    class PhpDocumentor {
+
+        /**
+         * @return array
+         * @since 2.0.2
+         * @author Dario Curvino <@dudo>
+         *
+         */
+        public static function returnTags(): array {
+            return array(
+                '@abstract',
+                '@access',
+                '@author',
+                '@category',
+                '@copyright',
+                '@deprecated',
+                '@example',
+                '@final',
+                '@filesource',
+                '@global',
+                '@ignore',
+                '@internal',
+                '@license',
+                '@link',
+                '@method',
+                '@name',
+                '@package',
+                '@param',
+                '@property',
+                '@return',
+                '@see',
+                '@since',
+                '@static',
+                '@staticvar',
+                '@subpackage',
+                '@todo',
+                '@tutorial',
+                '@uses',
+                '@var',
+                '@version'
+            );
         }
-        return false;
+
+        /**
+         * Return true if the given string is a phpDocTag, false otherwise
+         *
+         * @param $string
+         * @return bool
+         * @author Dario Curvino <@dudo>
+         *
+         * @since 2.0.2
+         */
+        public static function isTag($string): bool {
+            if (in_array($string, self::returnTags(), true)) {
+                return true;
+            }
+            return false;
+        }
     }
-}
+
+} //end class
